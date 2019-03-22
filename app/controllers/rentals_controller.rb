2a807794f149250fe9e24b7b1213c2670ad81d59
melanie rat <RAT.melanie@gmail.com>
2019-03-22 14:30:21 +0100

    Add total price

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -13,15 +13,20 @@ class RentalsController < ApplicationController
     @rental.castle = Castle.find(params[:castle_id])
     @rental.player = current_user
     if @rental.save
+      total_price
       redirect_to rentals_path
     else
       render :new
     end
   end
 
+  def total_price
+    (@rental.starts_on - @rental.ends_on) * @rental.castle.day_price
+  end
+
   private
 
   def rental_params
-    params.require(:rental).permit(:starts_on, :ends_on)
+    params.require(:rental).permit(:starts_on, :ends_on, :total_price)
   end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,6 +3,9 @@ class Rental < ApplicationRecord
   belongs_to :player, class_name: 'User'
   before_save :default_values
 
+  # validates :starts_on, presence: true
+  # validates :ends_on, presence: true
+
   def default_values
     if self.status.nil?
       self.status = 'pending'
